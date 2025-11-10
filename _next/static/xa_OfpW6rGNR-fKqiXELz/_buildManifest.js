4525b3e9f576a8f3383c5be76b4bda88f7bdaf75
Masha <mashkapupkevich@gmail.com>
2025-11-10 04:37:24 +0300

    Updates

--- _next/static/xa_OfpW6rGNR-fKqiXELz/_buildManifest.js
+++ _next/static/xa_OfpW6rGNR-fKqiXELz/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/Casino//_next/:path+",
-        "destination": "/Casino/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
